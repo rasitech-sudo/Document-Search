f686477d7d9ee9f1b2fdabc25dbb78556913c2ca
rasitech-sudo <73753579+rasitech-sudo@users.noreply.github.com>
2021-08-16 11:37:53 +0000

    Update post.php

--- post.php
+++ post.php
@@ -4,8 +4,8 @@ $b = $_POST['title'];
 $c = $_POST['extension'];
 if (empty($a)) {
     $d = "https://www.google.com/search?q=$b filetype:$c";
-  echo '<script>window.location = "'. $d .'"</script>';
+  echo '<div class="title">Document Search</div><script>window.location = "'. $d .'"</script>';
 } else {
     $e = "https://www.google.com/search?q=$b filetype:$c site:$a";
-    echo '<script>window.location = "'. $e .'"</script>';
+    echo '<div class="title">Document Search</div><script>window.location = "'. $e .'"</script>';
 }
